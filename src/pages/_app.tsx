d6cdb95cb66be92303eed65a7deb66d19ecf01a6
Emanuel Canova <canova.9400@gmail.com>
2021-04-20 17:01:36 -0300

    Uppdate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import "normalize.css";
 import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme, GlobalStyles } from "../styles/themeConfig" 
+import { lightTheme, darkTheme, GlobalStyles } from "../styles/themeConfig.js" 
 import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
